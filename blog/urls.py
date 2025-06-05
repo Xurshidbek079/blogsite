b58a0ce68179d89cfdcfd64ac09cfd07b837d7f8
Xurshidbek079 <x.ro'ziyev@student.inha.uz>
2025-06-05 14:30:32 +0500

    Complete blog redesign: new navigation, multiple sections, clean layout

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import BlogPostListView, BlogPostDetailView
+from .views import (
+    HomeView, BlogPostListView, BlogPostDetailView, 
+    AboutView, ChannelView, ProjectsView, BooksView, NowView, ToolsView
+)
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', BlogPostListView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', BlogPostListView.as_view(), name='blog_list'),
     path('post/<slug:slug>/', BlogPostDetailView.as_view(), name='post_detail'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('channel/', ChannelView.as_view(), name='channel'),
+    path('projects/', ProjectsView.as_view(), name='projects'),
+    path('books/', BooksView.as_view(), name='books'),
+    path('now/', NowView.as_view(), name='now'),
+    path('tools/', ToolsView.as_view(), name='tools'),
 ] 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import BlogPost
+
+class HomeView(TemplateView):
+    template_name = 'blog/home.html'
 
 class BlogPostListView(ListView):
     model = BlogPost
-    template_name = 'blog/home.html'
+    template_name = 'blog/blog_list.html'
     context_object_name = 'posts'
     paginate_by = 10
 
@@ -11,4 +14,22 @@ class BlogPostDetailView(DetailView):
     model = BlogPost
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
-    slug_url_kwarg = 'slug' 
+    slug_url_kwarg = 'slug'
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+class ChannelView(TemplateView):
+    template_name = 'blog/channel.html'
+
+class ProjectsView(TemplateView):
+    template_name = 'blog/projects.html'
+
+class BooksView(TemplateView):
+    template_name = 'blog/books.html'
+
+class NowView(TemplateView):
+    template_name = 'blog/now.html'
+
+class ToolsView(TemplateView):
+    template_name = 'blog/tools.html' 
